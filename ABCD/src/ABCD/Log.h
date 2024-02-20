621847681155ee9cf14169fd368cc08e76c731da
oakmura <jamicoder94@gmail.com>
2024-02-20 22:30:43 +0900

    fixed wrong spdlog api call

--- ABCD/src/ABCD/Log.h
+++ ABCD/src/ABCD/Log.h
@@ -24,10 +24,10 @@ namespace abcd
 #define AB_CORE_INFO(...) abcd::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define AB_CORE_WARN(...) abcd::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define AB_CORE_ERROR(...) abcd::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define AB_CORE_FATAL(...) abcd::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define AB_CORE_FATAL(...) abcd::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 #define AB_TRACE(...) abcd::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define AB_INFO(...) abcd::Log::GetClientLogger()->info(__VA_ARGS__)
 #define AB_WARN(...) abcd::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define AB_ERROR(...) abcd::Log::GetClientLogger()->error(__VA_ARGS__)
-#define AB_FATAL(...) abcd::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define AB_FATAL(...) abcd::Log::GetClientLogger()->critical(__VA_ARGS__)
