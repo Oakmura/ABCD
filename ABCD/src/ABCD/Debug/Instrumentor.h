19ec62176bdbcb247d51cc3340c5eb5a3878abcb
oakmura <jamicoder94@gmail.com>
2024-02-24 13:24:56 +0900

    Auto deducing an availabe __FUNCSIG__ definition

--- ABCD/src/ABCD/Debug/Instrumentor.h
+++ ABCD/src/ABCD/Debug/Instrumentor.h
@@ -127,10 +127,31 @@ namespace abcd
 
 #define AB_PROFILE 1
 #if AB_PROFILE
+    // Resolve which function signature macro will be used. Note that this only
+    // is resolved when the (pre)compiler starts, so the syntax highlighting
+    // could mark the wrong one in your editor!
+    #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+        #define AB_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__DMC__) && (__DMC__ >= 0x810)
+        #define AB_FUNC_SIG __PRETTY_FUNCTION__
+    #elif defined(__FUNCSIG__)
+        #define AB_FUNC_SIG __FUNCSIG__
+    #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+        #define AB_FUNC_SIG __FUNCTION__
+    #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+        #define AB_FUNC_SIG __FUNC__
+    #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+        #define AB_FUNC_SIG __func__
+    #elif defined(__cplusplus) && (__cplusplus >= 201103)
+        #define AB_FUNC_SIG __func__
+    #else
+        #define AB_FUNC_SIG "AB_FUNC_SIG unknown!"
+    #endif
+
     #define AB_PROFILE_BEGIN_SESSION(name, filepath) ::abcd::Instrumentor::Get().BeginSession(name, filepath)
     #define AB_PROFILE_END_SESSION() ::abcd::Instrumentor::Get().EndSession()
     #define AB_PROFILE_SCOPE(name) ::abcd::InstrumentationTimer timer##__LINE__(name);
-    #define AB_PROFILE_FUNCTION() AB_PROFILE_SCOPE(__FUNCSIG__)
+    #define AB_PROFILE_FUNCTION() AB_PROFILE_SCOPE(AB_FUNC_SIG)
 #else
     #define AB_PROFILE_BEGIN_SESSION(name, filepath)
     #define AB_PROFILE_END_SESSION()
