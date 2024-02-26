1183d29213fcbc74a548a7b6496d9fb3d4a50c2a
oakmura <jamicoder94@gmail.com>
2024-02-26 14:57:52 +0900

    Optimized instrumentation

--- ABCD/src/ABCD/Debug/Instrumentor.h
+++ ABCD/src/ABCD/Debug/Instrumentor.h
@@ -14,6 +14,7 @@ namespace abcd
     struct ProfileResult
     {
         std::string Name;
+
         FloatingPointMicroseconds Start;
         std::chrono::microseconds ElapsedTime;
         std::thread::id ThreadID;
@@ -39,28 +40,29 @@ namespace abcd
         void BeginSession(const std::string& name, const std::string& filepath = "results.json")
         {
             std::lock_guard lock(mMutex);
-            if (mCurrentSession) 
+            if (mCurrentSession)
             {
                 // If there is already a current session, then close it before beginning new one.
                 // Subsequent profiling output meant for the original session will end up in the
                 // newly opened session instead.  That's better than having badly formatted
                 // profiling output.
-                if (Log::GetCoreLogger()) 
-                { // Edge case: BeginSession() might be before Log::Init()
+                if (Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+                {
                     AB_CORE_ERROR("Instrumentor::BeginSession('{0}') when session '{1}' already open.", name, mCurrentSession->Name);
                 }
-                internalEndSession();
+                InternalEndSession();
             }
-
             mOutputStream.open(filepath);
-            if (mOutputStream.is_open()) 
+
+            if (mOutputStream.is_open())
             {
                 mCurrentSession = new InstrumentationSession({ name });
-                writeHeader();
+                WriteHeader();
             }
-            else {
-                if (Log::GetCoreLogger()) 
-                { // Edge case: BeginSession() might be before Log::Init()
+            else
+            {
+                if (Log::GetCoreLogger()) // Edge case: BeginSession() might be before Log::Init()
+                {
                     AB_CORE_ERROR("Instrumentor could not open results file '{0}'.", filepath);
                 }
             }
@@ -69,21 +71,18 @@ namespace abcd
         void EndSession()
         {
             std::lock_guard lock(mMutex);
-            internalEndSession();
+            InternalEndSession();
         }
 
         void WriteProfile(const ProfileResult& result)
         {
             std::stringstream json;
 
-            std::string name = result.Name;
-            std::replace(name.begin(), name.end(), '"', '\'');
-
             json << std::setprecision(3) << std::fixed;
             json << ",{";
             json << "\"cat\":\"function\",";
             json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-            json << "\"name\":\"" << name << "\",";
+            json << "\"name\":\"" << result.Name << "\",";
             json << "\"ph\":\"X\",";
             json << "\"pid\":0,";
             json << "\"tid\":" << result.ThreadID << ",";
@@ -91,39 +90,40 @@ namespace abcd
             json << "}";
 
             std::lock_guard lock(mMutex);
-            if (mCurrentSession) 
+            if (mCurrentSession)
             {
                 mOutputStream << json.str();
                 mOutputStream.flush();
             }
         }
 
-        static Instrumentor& Get() 
+        static Instrumentor& Get()
         {
             static Instrumentor instance;
             return instance;
         }
 
     private:
-        void writeHeader()
+
+        void WriteHeader()
         {
             mOutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
             mOutputStream.flush();
         }
 
-        void writeFooter()
+        void WriteFooter()
         {
             mOutputStream << "]}";
             mOutputStream.flush();
         }
 
-        // Note: you must already own lock on mMutex before
+        // Note: you must already own lock on m_Mutex before
         // calling InternalEndSession()
-        void internalEndSession() 
+        void InternalEndSession()
         {
-            if (mCurrentSession) 
+            if (mCurrentSession)
             {
-                writeFooter();
+                WriteFooter();
                 mOutputStream.close();
                 delete mCurrentSession;
                 mCurrentSession = nullptr;
@@ -144,17 +144,14 @@ namespace abcd
         ~InstrumentationTimer()
         {
             if (!mStopped)
-            {
                 Stop();
-            }
         }
 
         void Stop()
         {
             auto endTimepoint = std::chrono::steady_clock::now();
             auto highResStart = FloatingPointMicroseconds{ mStartTimepoint.time_since_epoch() };
-            auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - 
-                std::chrono::time_point_cast<std::chrono::microseconds>(mStartTimepoint).time_since_epoch();
+            auto elapsedTime = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch() - std::chrono::time_point_cast<std::chrono::microseconds>(mStartTimepoint).time_since_epoch();
 
             Instrumentor::Get().WriteProfile({ mName, highResStart, elapsedTime, std::this_thread::get_id() });
 
@@ -165,6 +162,35 @@ namespace abcd
         std::chrono::time_point<std::chrono::steady_clock> mStartTimepoint;
         bool mStopped;
     };
+
+    namespace InstrumentorUtils 
+    {
+        template <size_t N>
+        struct ChangeResult
+        {
+            char Data[N];
+        };
+
+        template <size_t N, size_t K>
+        constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+        {
+            ChangeResult<N> result = {};
+
+            size_t srcIndex = 0;
+            size_t dstIndex = 0;
+            while (srcIndex < N)
+            {
+                size_t matchIndex = 0;
+                while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+                    matchIndex++;
+                if (matchIndex == K - 1)
+                    srcIndex += matchIndex;
+                result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+                srcIndex++;
+            }
+            return result;
+        }
+    }
 }
 
 #define AB_PROFILE 0
@@ -176,7 +202,7 @@ namespace abcd
 #define AB_FUNC_SIG __PRETTY_FUNCTION__
 #elif defined(__DMC__) && (__DMC__ >= 0x810)
 #define AB_FUNC_SIG __PRETTY_FUNCTION__
-#elif defined(__FUNCSIG__)
+#elif (defined(__FUNCSIG__) || (_MSC_VER))
 #define AB_FUNC_SIG __FUNCSIG__
 #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
 #define AB_FUNC_SIG __FUNCTION__
@@ -192,7 +218,8 @@ namespace abcd
 
 #define AB_PROFILE_BEGIN_SESSION(name, filepath) ::abcd::Instrumentor::Get().BeginSession(name, filepath)
 #define AB_PROFILE_END_SESSION() ::abcd::Instrumentor::Get().EndSession()
-#define AB_PROFILE_SCOPE(name) ::abcd::InstrumentationTimer timer##__LINE__(name);
+#define AB_PROFILE_SCOPE(name) constexpr auto fixedName = ::abcd::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+									::abcd::InstrumentationTimer timer##__LINE__(fixedName.Data)
 #define AB_PROFILE_FUNCTION() AB_PROFILE_SCOPE(AB_FUNC_SIG)
 #else
 #define AB_PROFILE_BEGIN_SESSION(name, filepath)
